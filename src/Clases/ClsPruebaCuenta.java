efddbccb3208f945b1bb4e2f1be02d6514dc433f
Bryner <Bryner@DESKTOP-6IRJJ50>
2020-08-01 22:25:53 -0600

    Ejercicio_2

---
+++ src/Clases/ClsPruebaCuenta.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Clases;
+
+/**
+ *
+ * @author Bryner
+ */
+public class ClsPruebaCuenta {
+    
+}
